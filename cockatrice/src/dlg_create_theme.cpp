010c7e9861917d553e82f426a6e09484ef21277e
Ira Aspen <zebington@gmail.com>
2016-02-07 01:31:46 +0000

    Updated dialogue to have the barebones.

--- cockatrice/src/dlg_create_theme.cpp
+++ cockatrice/src/dlg_create_theme.cpp
@@ -1,6 +1,56 @@
+#include <QDialogButtonBox>
+#include <QListWidget>
+#include <QStackedWidget>
 #include <QVBoxLayout>
+
 #include "dlg_create_theme.h"
 
 DlgCreateTheme::DlgCreateTheme(QWidget *parent) : QDialog(parent)
 {
+    contentsWidget = new QListWidget;
+    contentsWidget->setViewMode(QListView::IconMode);
+    contentsWidget->setMovement(QListView::Static);
+    contentsWidget->setMinimumHeight(40);
+    contentsWidget->setMaximumHeight(40);
+    contentsWidget->setSpacing(5);
+
+    pagesWidget = new QStackedWidget;
+    pagesWidget->addWidget(new GeneralCreateThemePage);
+
+    createList();
+    contentsWidget->setCurrentRow(0);
+
+    QVBoxLayout *vboxLayout = new QVBoxLayout;
+    vboxLayout->addWidget(contentsWidget);
+    vboxLayout->addWidget(pagesWidget);
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(vboxLayout);
+    mainLayout->addSpacing(12);
+    mainLayout->addWidget(buttonBox);
+    setLayout(mainLayout);
+
+    retranslateUi();
+
+    adjustSize();
 }
+
+void DlgCreateTheme::createList()
+{
+    generalButton = new QListWidgetItem(contentsWidget);
+    generalButton->setTextAlignment(Qt::AlignHCenter);
+    generalButton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+}
+
+void DlgCreateTheme::retranslateUi()
+{
+    setWindowTitle(tr("Settings"));
+
+    generalButton->setText(tr("General"));
+
+    contentsWidget->reset();
+}
+
+GeneralCreateThemePage::GeneralCreateThemePage() {}
--- cockatrice/src/dlg_create_theme.h
+++ cockatrice/src/dlg_create_theme.h
@@ -2,11 +2,20 @@
 #define DLG_CREATE_THEME_H
 
 #include <QDialog>
+#include <QListWidget>
+#include <QListWidgetItem>
+#include <QStackedWidget>
 
 class DlgCreateTheme : public QDialog {
     Q_OBJECT
 public:
     DlgCreateTheme(QWidget *parent = 0);
+private:
+    QListWidget *contentsWidget;
+    QListWidgetItem *generalButton;
+    QStackedWidget *pagesWidget;
+    void createList();
+    void retranslateUi();
 };
 
 class AbstractCreateThemePage : public QWidget {};
